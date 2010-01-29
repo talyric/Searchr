243661ada6d9b6ada876de6bfa84923f1c5639b2
Andrew <talyric@gmail.com>
2010-01-29 14:33:03 -0800

    Added capibilities (many-to-many with users)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100129003041) do
+ActiveRecord::Schema.define(:version => 20100129215341) do
+
+  create_table "capabilities", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_capabilities", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "capability_id"
+  end
+
+  add_index "user_capabilities", ["capability_id"], :name => "index_user_capabilities_on_capability_id"
+  add_index "user_capabilities", ["user_id"], :name => "index_user_capabilities_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "crypted_password",          :limit => 40
